b8a6e5affc4893d48cf1b481161871fe7ada6192
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-07 15:00:44 +0100

    Create ClickableIconCardExit.kt

---
+++ Task_Management_Dungeon/src/main/kotlin/composable/ClickableIconCardExit.kt
@@ -0,0 +1,43 @@
+package composable
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import kotlin.system.exitProcess
+
+/**
+ * Card to exit the Programm
+ * @param icon ImageVector of the Icon that is shown in the card
+ * @param text Text that is shown in the Card
+ */
+@Composable
+fun clickableIconCardExit(icon : ImageVector, text : String) {
+    Card(elevation = 15.dp,modifier = Modifier.padding(10.dp).size(width = 120.dp, height = 120.dp).clickable {
+        exitProcess(0)
+    }, backgroundColor = Color.DarkGray, border = BorderStroke(1.dp, color = Color.Black))
+    {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(icon, "Icon", Modifier.padding(10.dp))
+            Text(
+                text = text,
+                modifier = Modifier.padding(2.dp),
+                color = Color.LightGray
+            )
+        }
+    }
+}
