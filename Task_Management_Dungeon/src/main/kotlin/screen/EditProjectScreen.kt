06f27bbf19e2308a256357dcd391b25212c782d8
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-08 12:23:12 +0100

    Refactor EditProjectScreen

---
+++ Task_Management_Dungeon/src/main/kotlin/screen/EditProjectScreen.kt
@@ -0,0 +1,27 @@
+package screen
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+
+class EditProjectScreen : Screen {
+    @Composable
+    @Preview
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+        MaterialTheme {
+            Button(onClick = {
+                navigator.pop()
+            }) {
+                "Zurück!"
+            }
+
+        }
+    }
+
+}
