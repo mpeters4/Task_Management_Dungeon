31f610b60c809f45d3eef0b033f06420cd9641d0
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-10 13:05:53 +0100

    Created specific Create Question Screens

---
+++ Task_Management_Dungeon/src/main/kotlin/screen/CreateAssignScreen.kt
@@ -0,0 +1,9 @@
+package screen
+
+import cafe.adriel.voyager.core.screen.Screen
+
+class CreateAssignScreen : Screen {
+    override fun Content() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ Task_Management_Dungeon/src/main/kotlin/screen/CreateSingleChoiceScreen.kt
@@ -0,0 +1,9 @@
+package screen
+
+import cafe.adriel.voyager.core.screen.Screen
+
+class CreateSingleChoiceScreen : Screen {
+    override fun Content() {
+        TODO("Not yet implemented")
+    }
+}
