bb818ef5519691624ad40838bceb5fbc08feafd0
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-10 13:07:17 +0100

    Update CreateSingleChoiceScreen.kt

--- Task_Management_Dungeon/src/main/kotlin/screen/CreateSingleChoiceScreen.kt
+++ Task_Management_Dungeon/src/main/kotlin/screen/CreateSingleChoiceScreen.kt
@@ -1,9 +1,35 @@
 package screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+import com.example.compose.AppTheme
+import composable.createAnswers
+import composable.inputTextField
 
 class CreateSingleChoiceScreen : Screen {
     override fun Content() {
-        TODO("Not yet implemented")
+        val navigator = LocalNavigator.currentOrThrow
+        AppTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                Column(/*Modifier.verticalScroll(rememberScrollState())*/) {
+                    inputTextField(Modifier)
+                    createAnswers(Modifier)
+                    Button(colors = ButtonDefaults.buttonColors(), onClick = {
+                        navigator.pop()
+                    }) {
+                        Text("Zurück!")
+                    }
+                }
+            }
+        }
+
     }
 }
