98727528d69198eecbba2cd79eabe80cb42b3577
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-10 09:51:18 +0100

    HomeScreen V1 finished

--- Task_Management_Dungeon/src/main/kotlin/screen/HomeScreen.kt
+++ Task_Management_Dungeon/src/main/kotlin/screen/HomeScreen.kt
@@ -1,21 +1,26 @@
+@file:OptIn(ExperimentalLayoutApi::class)
+
 package screen
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import com.example.compose.AppTheme
 import composable.clickableIconCardExit
 import composable.clickableIconCardNavigate
 import icon.Exit
@@ -23,31 +28,78 @@ import icon.RectangleList
 import icon.SquarePlus
 import icon.Walking
 
-class HomeScreen: Screen {
+class HomeScreen : Screen {
     @Composable
     @Preview
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
-        MaterialTheme {
+        AppTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                Column (
+                    Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background).padding(8.dp)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    Text(
+                        text = "Willkommen!",
+                        textAlign = TextAlign.Left,
+                        fontSize = 50.sp,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    Box(
+                        Modifier.fillMaxSize().padding(top = 30.dp)
+                    ) {
+                        FlowRow(
+                            Modifier.height(intrinsicSize = IntrinsicSize.Max)
+                                .width(intrinsicSize = IntrinsicSize.Max)
+                                .align(Alignment.Center),
+                            maxItemsInEachRow = 3
+                        ) {
+                            clickableIconCardNavigate(
+                                SquarePlus(MaterialTheme.colorScheme.onBackground),
+                                "Projekte",
+                                Modifier.padding(8.dp),
+                                navigator,
+                                EditProjectScreen()
+                            )
+                            clickableIconCardNavigate(
+                                Walking(MaterialTheme.colorScheme.onBackground),
+                                "Aufgabendatei\ngenerieren",
+                                Modifier.padding(8.dp),
+                                navigator,
+                                CreateTaskFileScreen()
+                            )
+                            clickableIconCardNavigate(
+                                SquarePlus(MaterialTheme.colorScheme.onBackground),
+                                "Neue Aufgabe",
+                                Modifier.padding(8.dp),
+                                navigator,
+                                CreateQuestionScreen()
+                            )
+                            clickableIconCardNavigate(
+                                Walking(MaterialTheme.colorScheme.onBackground),
+                                "Simulieren",
+                                Modifier.padding(8.dp),
+                                navigator,
+                                HomeScreen()
+                            )
+                            clickableIconCardNavigate(
+                                RectangleList(MaterialTheme.colorScheme.onBackground),
+                                "Fragenliste",
+                                Modifier.padding(8.dp),
+                                navigator,
+                                QuestionOverviewScreen()
+                            )
+                            clickableIconCardExit(
+                                Exit(MaterialTheme.colorScheme.onBackground),
+                                "Beenden",
+                                Modifier.padding(8.dp)
+                            )
+                        }
+                    }
 
-
-        }
-        Box(Modifier.fillMaxSize().background(Color.DarkGray)) {
-            Text("Willkommen!", fontSize = 50.sp, color = Color.LightGray)
-            Box(Modifier.align(Alignment.Center)) {
-                Row {
-                    Column {
-                        clickableIconCardNavigate(SquarePlus(), navigator, EditProjectScreen(), "Projekte")
-                        clickableIconCardNavigate(Walking(), navigator, CreateTaskFileScreen(), "Aufgabendatei generieren")
-                    }
-                    Column {
-                        clickableIconCardNavigate(SquarePlus(), navigator, CreateQuestionScreen(), "Neue Aufgabe")
-                        clickableIconCardNavigate(Walking(), navigator, HomeScreen(), "Simulieren")
-                    }
-                    Column {
-                        clickableIconCardNavigate(RectangleList(), navigator, QuestionOverviewScreen(), "Fragenliste")
-                        clickableIconCardExit(Exit(), "Beenden")
-                    }
                 }
             }
         }
