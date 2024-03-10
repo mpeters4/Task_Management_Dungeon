12de8a0aa04011abf649318f292eb8df0099308b
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-10 09:49:49 +0100

    Updated Cards

--- Task_Management_Dungeon/src/main/kotlin/composable/ClickableIconCardExit.kt
+++ Task_Management_Dungeon/src/main/kotlin/composable/ClickableIconCardExit.kt
@@ -1,19 +1,18 @@
 package composable
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Card
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import kotlin.system.exitProcess
 
@@ -23,20 +22,28 @@ import kotlin.system.exitProcess
  * @param text Text that is shown in the Card
  */
 @Composable
-fun clickableIconCardExit(icon : ImageVector, text : String) {
-    Card(elevation = 15.dp,modifier = Modifier.padding(10.dp).size(width = 120.dp, height = 120.dp).clickable {
-        exitProcess(0)
-    }, backgroundColor = Color.DarkGray, border = BorderStroke(1.dp, color = Color.Black))
-    {
+fun clickableIconCardExit(
+    icon: ImageVector,
+    text: String,
+    modifier: Modifier
+) {
+    androidx.compose.material3.Card(
+        modifier = modifier.clickable { exitProcess(0) },
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background
+        ),
+    ) {
         Column(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
+
         ) {
             Image(icon, "Icon", Modifier.padding(10.dp))
             Text(
-                text = text,
-                modifier = Modifier.padding(2.dp),
-                color = Color.LightGray
+                text,
+                style = MaterialTheme.typography.titleLarge,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
             )
         }
     }
--- Task_Management_Dungeon/src/main/kotlin/composable/ClickableIconCardNavigate.kt
+++ Task_Management_Dungeon/src/main/kotlin/composable/ClickableIconCardNavigate.kt
@@ -1,19 +1,19 @@
 package composable
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Card
-import androidx.compose.material.Text
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.Navigator
@@ -26,20 +26,29 @@ import cafe.adriel.voyager.navigator.Navigator
  * @param location Screen to navigate to
  */
 @Composable
-fun clickableIconCardNavigate(icon : ImageVector, navigator: Navigator, location: Screen, text : String) {
-    Card(elevation = 15.dp,modifier = Modifier.padding(10.dp).size(width = 120.dp, height = 120.dp).clickable {
-        navigator.push(location)
-    }, backgroundColor = Color.DarkGray, border = BorderStroke(1.dp, color = Color.Black))
-    {
+fun clickableIconCardNavigate(
+    icon: ImageVector,
+    text: String,
+    modifier: Modifier,
+    navigator: Navigator,
+    nextLocation: Screen
+) {
+    Card(
+        modifier = modifier.clickable { navigator.push(nextLocation) },
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background
+        ),
+    ) {
         Column(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Image(icon, "Icon", Modifier.padding(10.dp))
             Text(
-                text = text,
-                modifier = Modifier.padding(2.dp),
-                color = Color.LightGray
+                text,
+                style = MaterialTheme.typography.titleLarge,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
             )
         }
     }
