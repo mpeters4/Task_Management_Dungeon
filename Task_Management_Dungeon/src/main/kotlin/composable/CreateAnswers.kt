8cc2b3d95214a554c860ebb2a9793e4561d5be6c
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-11 16:42:31 +0100

    Update CreateAnswers.kt

--- Task_Management_Dungeon/src/main/kotlin/composable/CreateAnswers.kt
+++ Task_Management_Dungeon/src/main/kotlin/composable/CreateAnswers.kt
@@ -5,16 +5,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import icon.addIcon
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,34 +21,52 @@ fun createAnswers(
     modifier: Modifier
 ) {
     var text by rememberSaveable { mutableStateOf("") }
-    var answers = remember { mutableStateListOf<String>()}
-    Column {
+    var answers = remember { mutableStateListOf<String>() }
+    Column() {
 
         Text(
             text = "Bitte geben Sie die möglichen Antworten ein:",
             style = MaterialTheme.typography.titleLarge,
             textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.onSurfaceVariant)
-        Row {
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+        Row(content = {
+            Image(
+                addIcon(MaterialTheme.colorScheme.onBackground),
+                "Add",
+                Modifier.padding(4.dp, top = 26.dp).clickable {
+                    answers.add(text)
+                    text = ""
+                })
             OutlinedTextField(
-                modifier = modifier.padding(8.dp),
+                modifier = modifier.padding(8.dp).fillMaxWidth(),
                 value = text,
                 onValueChange = { text = it },
                 label = { Text("Antwort eingeben") }
             )
-            Image(addIcon(MaterialTheme.colorScheme.onBackground), "Add", Modifier.padding(4.dp, top = 26.dp).clickable {
-                answers.add(text)
-                text = ""
-            })
-        }
+
+        })
         Text(
             text = "Antworten:",
             style = MaterialTheme.typography.titleLarge,
             textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.onSurfaceVariant)
-        LazyColumn {
-            items(items = answers) {
-                    answer -> Text("Antwort: " + answer, modifier = Modifier.clickable { answers.remove(answer) })
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+        LazyColumn() {
+            items(items = answers) { answer ->
+                Card(
+                    modifier= modifier.fillMaxWidth(),
+                    colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.onSecondary
+                )){
+                    Box(){
+                        Row(modifier.align(Alignment.Center),
+                            verticalAlignment = Alignment.CenterVertically) {
+                            Image(addIcon(MaterialTheme.colorScheme.onSurfaceVariant), "Remove Item", Modifier.padding(10.dp).clickable { answers.remove(answer)})
+                            Text("Antwort: " + answer,style = MaterialTheme.typography.bodyMedium, fontSize = 24.sp, lineHeight = 25.sp, modifier = modifier.clickable {})
+                        }
+                    }
+                }
             }
         }
     }
