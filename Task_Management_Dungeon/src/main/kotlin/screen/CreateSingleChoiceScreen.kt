1c986699f92a1bc3c66dce9b4d08835b9add9568
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-12 13:34:29 +0100

    Number Fields implemented

--- Task_Management_Dungeon/src/main/kotlin/screen/CreateSingleChoiceScreen.kt
+++ Task_Management_Dungeon/src/main/kotlin/screen/CreateSingleChoiceScreen.kt
@@ -1,11 +1,13 @@
 package screen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -13,54 +15,83 @@ import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import classes.SingleTaskQuestion
 import com.example.compose.AppTheme
 import composable.createAnswers
+import composable.inputNumberField
 import composable.inputTextField
 import composable.inputTextFieldValueTest
 
 class CreateSingleChoiceScreen : Screen {
     @Composable
     override fun Content() {
+
         val navigator = LocalNavigator.currentOrThrow
+        //Variablen für Single Choice Frage
         var questionText by rememberSaveable { mutableStateOf("") }
         var answers = remember { mutableStateListOf<String>() }
-        var points by rememberSaveable { mutableStateOf(1) }
+        var points by rememberSaveable { mutableStateOf("") }
         var pointsToPass by rememberSaveable { mutableStateOf(1) }
         var explanation by rememberSaveable { mutableStateOf("") }
         var tags = remember { mutableStateListOf<String>() }
         var text by rememberSaveable { mutableStateOf("knopp") }
-
+        var correctAnswerIndex by rememberSaveable{ mutableStateOf(0) }
+        var task = SingleTaskQuestion()
         AppTheme {
             Surface(
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background,
             ) {
-                Column(Modifier.padding(start = 24.dp, top = 24.dp, end = 24.dp)/*Modifier.verticalScroll(rememberScrollState())*/) {
-                    Text("Single-Choice Frage erstellen:",
+                LazyColumn(Modifier.padding(start = 24.dp, top = 24.dp, end = 24.dp)/*Modifier.verticalScroll(rememberScrollState())*/) {
+                    item {  Text("Single-Choice Frage erstellen:",
                         style = MaterialTheme.typography.titleLarge,
                         textAlign = TextAlign.Center,
                         fontSize = 50.sp,
                         color = MaterialTheme.colorScheme.onSurfaceVariant,
-                        modifier = Modifier.padding(bottom = 16.dp))
-                    inputTextField(Modifier)
-                    createAnswers(Modifier)
-                    inputTextFieldValueTest(Modifier,explanation, onValueChange = {explanation = it}, "TEST")
-                    inputTextFieldValueTest(Modifier,questionText, onValueChange = {questionText = it}, "Rissa")
-                    Button(colors = ButtonDefaults.buttonColors(), onClick = {
-                        navigator.pop()
-                    }) {
-                        Text("Zurück!")
-                    }
-                    Button(colors = ButtonDefaults.buttonColors(), onClick = {
+                        modifier = Modifier.padding(bottom = 16.dp))}
+                    item {  inputTextField(Modifier, questionText, onValueChange = {questionText = it}, "Frage eingeben")}
+                    item { createAnswers(Modifier)}
+                    item { inputTextField(Modifier,explanation, onValueChange = {explanation = it}, "TEST")}
+                    item {  inputTextField(Modifier,questionText, onValueChange = {questionText = it}, "Rissa")}
+                    item {
+                        Row() {
+                            inputNumberField(Modifier.width(200.dp), points,onValueChange = {points = it}, "PUNKTE")
+                            inputNumberField(Modifier.width(200.dp), points,onValueChange = {points = it}, "PUNKTE")
+                        }
+                        }
+
+                    item {
+                        Row (//verticalAlignment = Alignment.Bottom,
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.End){
+                            Button(modifier = Modifier.padding(16.dp), colors = ButtonDefaults.buttonColors(), onClick = {
+                                navigator.pop()
+                            }) {
+                                Text("Zurück")
+                            }
+                            Button(modifier = Modifier.padding(16.dp), colors = ButtonDefaults.buttonColors(), onClick = {
+                                navigator.pop()
+                            }) {
+                                Text("Weiter")
+                            }
+                        } }
+                    //item {  }
+                    //USELESS Buttons
+                    item { Button(colors = ButtonDefaults.buttonColors(), onClick = {
                         text = explanation
                     }) {
                         Text(text)
-                    }
-                    Button(colors = ButtonDefaults.buttonColors(), onClick = {
-                        text = questionText
+                    }}
+                    item { Button(colors = ButtonDefaults.buttonColors(), onClick = {
+                        text = task.points.toString()
                     }) {
-                        Text(questionText)
-                    }
+                        task.points += 5
+
+                        Text(text)
+                    }}
+                    item {
+
+                        }
                 }
             }
         }
