017cc1fd9ddb4acc3f6543c2f612a2fc92109f7f
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-12 13:32:35 +0100

    Update SingleTaskQuestion.kt

--- Task_Management_Dungeon/src/main/kotlin/classes/SingleTaskQuestion.kt
+++ Task_Management_Dungeon/src/main/kotlin/classes/SingleTaskQuestion.kt
@@ -1,14 +1,23 @@
 package classes
 
+import androidx.compose.runtime.mutableStateListOf
+
 class SingleTaskQuestion(
-    questionID: Int,
-    description: String,
-    points: Int,
-    pointsToPass: Int,
-    explanation: String,
-    val answers: List<Answer>,
-    val correctAnswerIndex: Int,
-    tags: List<String>
-) : Question(questionID, description, points, pointsToPass, explanation, tags) {
+    
+) {
+
+    var correctAnswerIndex : Int = -1
+    var questionID : Int = 0
+        get() = field
+        set(value) {
+            field = value
+        }
+    var description: String = ""
+    var points: Int = 0
+    var pointsToPass: Int = 0
+    var explanation: String = ""
+    val answers: List<String> = mutableStateListOf<String>()
+    var tags: List<String> = mutableStateListOf<String>()
+
 
 }
