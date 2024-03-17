773a1b30699092833beab4b21c583afbca947416
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-17 15:26:21 +0100

    Update SingleChoiceChooseAnswerIndexScreen.kt

--- Task_Management_Dungeon/src/main/kotlin/screen/SingleChoiceChooseAnswerIndexScreen.kt
+++ Task_Management_Dungeon/src/main/kotlin/screen/SingleChoiceChooseAnswerIndexScreen.kt
@@ -1,42 +1,101 @@
 package screen
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import classes.SingleTaskQuestion
+import com.example.compose.AppTheme
 
-class SingleChoiceChooseAnswerIndexScreen(val question: SingleTaskQuestion) :Screen {
+class SingleChoiceChooseAnswerIndexScreen(val question: SingleTaskQuestion) : Screen {
 
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
-        LazyColumn {
-            item {Text("Bitte wählen Sie die korrekte Antwort aus:")  }
-            item {Text(question.description)}
-            item {Text(question.explanation)}
-            item {Text(question.points.toString())}
-            item {Text(question.pointsToPass.toString())}
-            items(items = question.answers){answer ->
-                Text(text = answer, color = MaterialTheme.colorScheme.onSurfaceVariant, modifier = Modifier.clickable {
+        AppTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background,
+            ) {
+                LazyColumn(
+                    Modifier.padding(
+                        start = 24.dp,
+                        top = 24.dp,
+                        end = 24.dp
+                    )
+                ) {
 
+                }
+            }
+        }
+        LazyColumn {
+            item {
+                Text(
+                    "Bitte kontrollieren Sie die Angaben und wählen Sie die korrekte Antwort aus der Antwortliste an:",
+                    style = MaterialTheme.typography.titleLarge,
+                    textAlign = TextAlign.Center,
+                    fontSize = 50.sp,
+                    lineHeight = 50.sp,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+            }
+            item {
+                Text(
+                    question.description,
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Center,
+                    fontSize = 50.sp,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+            }
+            item { Text(question.explanation,
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center,
+                fontSize = 50.sp,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                modifier = Modifier.padding(bottom = 16.dp)) }
+            item { Text(question.points.toString(),
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center,
+                fontSize = 50.sp,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                modifier = Modifier.padding(bottom = 16.dp)) }
+            item { Text(question.pointsToPass.toString(),
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center,
+                fontSize = 50.sp,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                modifier = Modifier.padding(bottom = 16.dp)) }
+            items(items = question.answers) { answer ->
+                Text(text = answer,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    modifier = Modifier.clickable {
                 })
+            }
+            items(items = question.tags) { tag ->
+                Text(text = tag,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    modifier = Modifier.clickable {
+                })
+            }
+            item {
                 Button(modifier = Modifier.padding(16.dp), colors = ButtonDefaults.buttonColors(), onClick = {
                     navigator.pop()
                 }) {
                     Text("Back")
                 }
-
             }
 
 
