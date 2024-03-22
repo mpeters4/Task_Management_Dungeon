12c0d57dbb8988b4883cf1a7acf05869d7945fe5
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-22 17:45:03 +0100

    refactor bodyText

--- Task_Management_Dungeon/src/main/kotlin/composable/TextForBody.kt
+++ Task_Management_Dungeon/src/main/kotlin/composable/TextForBody.kt
@@ -6,18 +6,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun bodyText(text : String){
+fun bodyText(text : String, size: Int = 16, modifier: Modifier = Modifier){
     Text(
         text = text,
         style = MaterialTheme.typography.bodyMedium,
-        textAlign = TextAlign.Center,
-        fontSize = 16.sp,
-        lineHeight = 16.sp,
+        textAlign = TextAlign.Start,
+        fontSize = size.sp,
+        lineHeight = size.sp,
         color = MaterialTheme.colorScheme.onSurfaceVariant,
-        modifier = Modifier.padding(bottom = 16.dp)
+        modifier = modifier.padding(bottom = 16.dp)
     )
 }
