f935ae1c5e49fb22b87b7cec6e2ad21e9ba53e13
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-22 17:49:17 +0100

    Finished DisplayQuestion for Single and Multiple

--- Task_Management_Dungeon/src/main/kotlin/composable/DisplayQuestion.kt
+++ Task_Management_Dungeon/src/main/kotlin/composable/DisplayQuestion.kt
@@ -2,61 +2,114 @@ package composable
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import classes.AssignQuestion
 import classes.MultipleChoiceQuestion
 import classes.SingleTaskQuestion
 
 @Composable
-@Preview
-fun QuestionDisplay(question: SingleTaskQuestion, modifier: Modifier){
-    Column(modifier = modifier.padding(16.dp).background(MaterialTheme.colorScheme.onBackground)) {
-        Text(question.description,
-            style = MaterialTheme.typography.bodyMedium,
-            textAlign = TextAlign.Center,
-            fontSize = 50.sp,
-            color = MaterialTheme.colorScheme.onSurfaceVariant,
-            modifier = Modifier.padding(bottom = 16.dp))
-        Text("Antworten:",
-            style = MaterialTheme.typography.bodyMedium,
-            textAlign = TextAlign.Center,
-            fontSize = 50.sp,
-            color = MaterialTheme.colorScheme.onSurfaceVariant,
-            modifier = Modifier.padding(bottom = 16.dp))
-        Column {
-            question.answers.forEach { answer ->
-                Text(answer,
-                    style = MaterialTheme.typography.bodyMedium,
-                    textAlign = TextAlign.Center,
-                    fontSize = 50.sp,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant,
-                    modifier = Modifier.padding(bottom = 16.dp))
+fun QuestionDisplay(question: SingleTaskQuestion, modifier: Modifier = Modifier){
+    Box (modifier.fillMaxSize().clip(shape = RoundedCornerShape(10.dp)).background(MaterialTheme.colorScheme.onSecondary)){
+        Column(modifier = modifier.fillMaxWidth().padding(16.dp)) {
+            Row {
+                bodyText("Frage: ", modifier =  Modifier.weight(1f))
+                bodyText(question.description, modifier = Modifier.weight(4f))
+            }
+            Row {
+                bodyText("Antworten:", modifier = Modifier.weight(1f))
+                Column(modifier = Modifier.weight(4f)) {
+                    question.answers.forEachIndexed { index, answer ->
+                        Row {
+                            bodyText("Antwort ${index+1}: ")
+                            bodyText(answer)
+                        }
+                    }
+                }
+            }
+            Row {
+                bodyText("Korrekte Antwort:", modifier = Modifier.weight(1f))
+                bodyText("Antwort ${question.correctAnswerIndex + 1}", modifier = Modifier.weight(4f))
+            }
+            Row {
+                bodyText("Erklärung:", modifier =  Modifier.weight(1f))
+                bodyText(question.explanation, modifier = Modifier.weight(4f))
+            }
+            Row {
+                bodyText("Punkte:", modifier = Modifier.weight(1f))
+                bodyText("$${question.points} (Punkte zum Bestehen: ${question.pointsToPass})", modifier = Modifier.weight(4f))
+            }
+            Row {
+                bodyText("Tags:", 20, Modifier.weight(1f))
+                if(question.tags.isNotEmpty()){
+                    Column(Modifier.weight(4f)) {
+                        question.tags.forEach { tag ->
+                            bodyText(tag)
+                        }
+                    }
+                }else{
+                    bodyText("Keine Tags vorhanden", modifier = Modifier.weight(4f))
+                }
             }
         }
-        Text("Antworten:",
-            style = MaterialTheme.typography.bodyMedium,
-            textAlign = TextAlign.Center,
-            fontSize = 50.sp,
-            color = MaterialTheme.colorScheme.onSurfaceVariant,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-
-
     }
-
 }
 
 @Composable
-fun QuestionDisplay(question: MultipleChoiceQuestion){
+fun QuestionDisplay(question: MultipleChoiceQuestion, modifier: Modifier = Modifier){
+    Box (modifier.fillMaxSize().clip(shape = RoundedCornerShape(10.dp)).background(MaterialTheme.colorScheme.onSecondary)){
+        Column(modifier = modifier.fillMaxWidth().padding(16.dp)) {
+            Row {
+                bodyText("Frage: ", modifier =  Modifier.weight(1f))
+                bodyText(question.description, modifier = Modifier.weight(4f))
+            }
+            Row {
+                bodyText("Antworten:", modifier = Modifier.weight(1f))
+                Column(modifier = Modifier.weight(4f)) {
+                    question.answers.forEachIndexed { index, answer ->
+                        Row {
+                            bodyText("Antwort ${index+1}: ")
+                            bodyText(answer)
+                        }
+                    }
+                }
+            }
+            Row {
+                bodyText("Korrekte Antworten:", modifier = Modifier.weight(1f))
+                Column(modifier = Modifier.weight(4f)) {
+                    question.correctAnswers.forEach(){  index ->
+                        bodyText("Antwort ${index+1}: ")
+                    }
+                }
 
+            }
+            Row {
+                bodyText("Erklärung:", modifier =  Modifier.weight(1f))
+                bodyText(question.explanation, modifier = Modifier.weight(4f))
+            }
+            Row {
+                bodyText("Punkte:", modifier = Modifier.weight(1f))
+                bodyText("$${question.points} (Punkte zum Bestehen: ${question.pointsToPass})", modifier = Modifier.weight(4f))
+            }
+            Row {
+                bodyText("Tags:", 20, Modifier.weight(1f))
+                if(question.tags.isNotEmpty()){
+                    Column(Modifier.weight(4f)) {
+                        question.tags.forEach { tag ->
+                            bodyText(tag)
+                        }
+                    }
+                }else{
+                    bodyText("Keine Tags vorhanden", modifier = Modifier.weight(4f))
+                }
+            }
+        }
+    }
 }
 @Composable
 fun QuestionDisplay(question: AssignQuestion){
