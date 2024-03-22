0a42c0651998db5efc6fe37517fa32275daa3420
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-22 17:48:17 +0100

    using bodyText

--- Task_Management_Dungeon/src/main/kotlin/composable/CreateStringList.kt
+++ Task_Management_Dungeon/src/main/kotlin/composable/CreateStringList.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,14 +31,8 @@ fun createStringList(
 ) {
     var text by rememberSaveable { mutableStateOf("") }
     val strings = remember { mutableStateListOf<String>() }
-
     Column {
-        Text(
-            text = taskLabel,
-            style = MaterialTheme.typography.titleLarge,
-            textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.onSurfaceVariant
-        )
+        bodyText(taskLabel)
         Row(content = {
             Image(
                 addIcon(MaterialTheme.colorScheme.onBackground),
@@ -55,7 +51,7 @@ fun createStringList(
                 isError = strings.size < minAmount
             )
         })
-        LazyColumn(Modifier.fillMaxWidth().size(200.dp).background(MaterialTheme.colorScheme.onSecondary)) {
+        LazyColumn(Modifier.fillMaxWidth().size(200.dp).clip(shape = RoundedCornerShape(10.dp)).background(MaterialTheme.colorScheme.onSecondary),) {
             item {
                 Text(
                     text = outputLabel,
