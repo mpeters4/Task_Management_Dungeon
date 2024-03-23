d8ea32c6d08e4087a4de37d2bd71bf6d68059747
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-23 12:03:09 +0100

    Update CreateMultipleChoiceScreen.kt
    Redone Single Choice Screen with some changes

--- Task_Management_Dungeon/src/main/kotlin/screen/CreateMultipleChoiceScreen.kt
+++ Task_Management_Dungeon/src/main/kotlin/screen/CreateMultipleChoiceScreen.kt
@@ -1,11 +1,166 @@
 package screen
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+import classes.MultipleChoiceQuestion
+import classes.SingleChoiceQuestion
+import com.example.compose.AppTheme
+import composable.createStringList
+import composable.inputNumberField
+import composable.inputTextField
+import composable.title
+import kotlinx.coroutines.launch
 
-class CreateMultipleChoiceScreen : Screen {
+/**
+ * Screen to check the Question before saving it to the Database
+ * @param question Multiple Choice Question created in Screen before
+ */
+class CreateMultipleChoiceScreen(val question: MultipleChoiceQuestion) : Screen {
     @Composable
     override fun Content() {
-        TODO("Not yet implemented")
+        val navigator = LocalNavigator.currentOrThrow
+        val snackbarHostState = remember { SnackbarHostState() }
+        val scope = rememberCoroutineScope()
+        //Variablen für Single Choice Frage
+        var questionText by rememberSaveable { mutableStateOf("") }
+        var answers = remember { mutableStateListOf<String>() }
+        var points by rememberSaveable { mutableStateOf("") }
+        var pointsToPass by rememberSaveable { mutableStateOf("") }
+        var explanation by rememberSaveable { mutableStateOf("") }
+        var tags = remember { mutableStateListOf<String>() }
+        AppTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background,
+            ) {
+                Scaffold(
+                    snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+                ) {
+                    LazyColumn(
+                        Modifier.padding(
+                            start = 48.dp,
+                            top = 20.dp,
+                            end = 48.dp
+                        )
+                    ) {
+                        item {
+                            title("Multiple Choice Frage erstellen")
+                        }
+                        //Multiple_Choice_Frage erstellen
+                        item {
+                            inputTextField(
+                                Modifier,
+                                questionText,
+                                onValueChange = { questionText = it },
+                                "Frage eingeben"
+                            )
+                        }
+                        item {
+                            createStringList(
+                                Modifier,
+                                onValueChange = { answers = it },
+                                taskLabel = "Bitte mindestens 2 Antworten angeben",
+                                outputLabel = "Antworten:",
+                                textFieldLabel = "Antwort angeben",
+                                minAmount = 2
+                            )
+                        }
+                        item {
+                            createStringList(
+                                Modifier,
+                                onValueChange = { tags = it },
+                                taskLabel = "Bitte Tags angeben",
+                                outputLabel = "Tags:",
+                                textFieldLabel = "Tag angeben",
+                                minAmount = 0
+                            )
+                        }
+                        item {
+                            inputTextField(
+                                Modifier,
+                                explanation,
+                                onValueChange = { explanation = it },
+                                "Erklärung angeben"
+                            )
+                        }
+                        item {
+                            Row {
+                                inputNumberField(
+                                    Modifier.width(300.dp),
+                                    points,
+                                    onValueChange = { points = it },
+                                    "Punkte"
+                                )
+                                inputNumberField(
+                                    Modifier.width(300.dp),
+                                    points,
+                                    onValueChange = { pointsToPass = it },
+                                    "Punkte zum bestehen"
+                                )
+                            }
+                        }
+
+                        item {
+                            Row(//verticalAlignment = Alignment.Bottom,
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.End
+                            ) {
+                                Button(
+                                    modifier = Modifier.padding(16.dp),
+                                    colors = ButtonDefaults.buttonColors(),
+                                    onClick = {
+                                        navigator.pop()
+                                    }) {
+                                    Text("Zurück")
+                                }
+                                Button(
+                                    modifier = Modifier.padding(16.dp),
+                                    colors = ButtonDefaults.buttonColors(),
+                                    onClick = {
+                                        if (questionText.isNotEmpty() && points.isNotEmpty() && explanation.isNotEmpty() && pointsToPass.isNotEmpty()) {
+                                            navigator.push(
+                                                SingleChoiceChooseAnswerIndexScreen(
+                                                    SingleChoiceQuestion(
+                                                        questionText,
+                                                        points.toInt(),
+                                                        pointsToPass.toInt(),
+                                                        explanation,
+                                                        answers,
+                                                        tags
+                                                    )
+                                                )
+                                            )
+                                        } else if (answers.size < 2) {
+                                            scope.launch {
+                                                snackbarHostState.showSnackbar(
+                                                    message = "Bitte geben Sie mindestens 2 Antwortmöglichkeiten an",
+                                                    withDismissAction = true
+                                                )
+                                            }
+                                        } else {
+                                            scope.launch {
+                                                snackbarHostState.showSnackbar(
+                                                    message = "Bitte füllen Sie alle Felder aus",
+                                                    withDismissAction = true
+                                                )
+                                            }
+                                        }
+                                    }) {
+                                    Text("Weiter")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
