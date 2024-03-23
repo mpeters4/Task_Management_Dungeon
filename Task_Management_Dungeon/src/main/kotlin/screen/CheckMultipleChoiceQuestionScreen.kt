bb28ce8148494da07002f5be7d1eddfe40911a11
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-23 12:01:56 +0100

    Create CheckMultipleChoiceQuestionScreen.kt

---
+++ Task_Management_Dungeon/src/main/kotlin/screen/CheckMultipleChoiceQuestionScreen.kt
@@ -0,0 +1,11 @@
+package screen
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+class CheckMultipleChoiceQuestionScreen: Screen {
+    @Composable
+    override fun Content() {
+
+    }
+}
