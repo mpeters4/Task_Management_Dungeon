62c02f24758b1f8626c0c616e7230a3da73715fd
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-23 15:59:15 +0100

    Create CheckAssignTaskScreen.kt
    Composable to create Assignments

---
+++ Task_Management_Dungeon/src/main/kotlin/screen/CheckAssignTaskScreen.kt
@@ -0,0 +1,12 @@
+package screen
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+class CheckAssignTaskScreen : Screen{
+    @Composable
+    override fun Content() {
+        TODO("Not yet implemented")
+    }
+
+}
