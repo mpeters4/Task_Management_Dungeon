985cf6b934b58aff066428f7170b64b0103fb435
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-26 18:27:11 +0100

    Update EditProjectScreen.kt

--- Task_Management_Dungeon/src/main/kotlin/screen/EditProjectScreen.kt
+++ Task_Management_Dungeon/src/main/kotlin/screen/EditProjectScreen.kt
@@ -1,27 +1,39 @@
 package screen
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import classes.Project
 import com.example.compose.AppTheme
+import composable.bodyText
+import composable.dropdownSelection
+import composable.expandableItem
 import composable.title
+import icon.addIcon
 
-class EditProjectScreen : Screen {
+class EditProjectScreen(val project: Project) : Screen {
     @Composable
     @Preview
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
+        val dependencies = mutableStateListOf("Sequenz", "Wenn Richtig, dann", "Wenn Falsch, dann", "Optionale Unteraufgabe", "Pflicht Unteraufgabe")
+        var selectedIndex by remember { mutableStateOf(0) }
         AppTheme {
             Surface(
                 modifier = Modifier.fillMaxSize(),
@@ -29,7 +41,7 @@ class EditProjectScreen : Screen {
             ) {
                 Scaffold(
                     topBar = {
-                        title("Aufgabendatei erstellen")
+                        title(project.name)
                     },
                     bottomBar = {
                         Row(//verticalAlignment = Alignment.Bottom,
@@ -46,8 +58,37 @@ class EditProjectScreen : Screen {
                             }
                         }
                     }
-
                 ) {
+                    LazyColumn(Modifier.padding(it)) {
+                        item {
+                            title("Spielablauf:")
+                        }
+                        item {
+                            dropdownSelection(dependencies, selectedIndex = selectedIndex, modifier = Modifier, {})
+
+                        }
+                        items(items = project.dependencies) {dependency ->
+                            Row {
+                                expandableItem(dependency.questionA,{})
+                                dropdownSelection(dependencies, selectedIndex = selectedIndex, modifier = Modifier, {})
+                                expandableItem(dependency.questionB,{})
+                            }
+
+
+                        }
+
+                        item { Row(Modifier.padding(start = 24.dp, end = 24.dp).fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                            Image(
+                                addIcon(androidx.compose.material3.MaterialTheme.colorScheme.onBackground),
+                                "Add",
+                                Modifier.padding(4.dp, top = 26.dp).clickable {
+                                    navigator.push(QuestionChooserScreen())
+                                })
+                            bodyText("Neue Frage hinzufügen")
+                        } }
+
+
+                    }
                 }
 
             }
