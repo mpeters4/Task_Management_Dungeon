0b13e8d6975ac664ce206a558f74eb2fad884cef
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-26 16:32:08 +0100

    Update EditProjectScreen.kt
    Basic Changes

--- Task_Management_Dungeon/src/main/kotlin/screen/EditProjectScreen.kt
+++ Task_Management_Dungeon/src/main/kotlin/screen/EditProjectScreen.kt
@@ -1,27 +1,56 @@
 package screen
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import com.example.compose.AppTheme
+import composable.title
 
 class EditProjectScreen : Screen {
     @Composable
     @Preview
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
-        MaterialTheme {
-            Button(onClick = {
-                navigator.pop()
-            }) {
-                "Zurück!"
-            }
+        AppTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = androidx.compose.material3.MaterialTheme.colorScheme.background,
+            ) {
+                Scaffold(
+                    topBar = {
+                        title("Aufgabendatei erstellen")
+                    },
+                    bottomBar = {
+                        Row(//verticalAlignment = Alignment.Bottom,
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Start
+                        ) {
+                            androidx.compose.material3.Button(
+                                modifier = Modifier.padding(16.dp),
+                                colors = ButtonDefaults.buttonColors(),
+                                onClick = {
+                                    navigator.pop()
+                                }) {
+                                androidx.compose.material3.Text("Zurück")
+                            }
+                        }
+                    }
 
+                ) {
+                }
+
+            }
         }
     }
-
 }
