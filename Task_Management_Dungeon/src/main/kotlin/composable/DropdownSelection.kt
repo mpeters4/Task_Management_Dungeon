8eef7dd2d1782a7a1377cf127fefc6e5093cc004
Michi <50985676+mpeters4@users.noreply.github.com>
2024-03-27 16:59:34 +0100

    complete overhaul on Dropdown

--- Task_Management_Dungeon/src/main/kotlin/composable/DropdownSelection.kt
+++ Task_Management_Dungeon/src/main/kotlin/composable/DropdownSelection.kt
@@ -1,87 +1,59 @@
+
 package composable
 
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Divider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Popup
-import androidx.compose.ui.window.PopupProperties
-import java.awt.Color
 
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
 @Composable
 fun dropdownSelection(
     itemList: List<String>,
-    selectedIndex: Int,
     modifier: Modifier,
-    onItemClick: (Int) -> Unit) {
-    var showDropdown by rememberSaveable { mutableStateOf(true) }
+    onItemClick: (Int) -> Unit
+) {
+    var showDropdown by rememberSaveable { mutableStateOf(false) }
+    var selectedIndex by rememberSaveable { mutableStateOf(0) }
     val scrollState = rememberScrollState()
 
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
-        Box(
+
+    ExposedDropdownMenuBox(
+        expanded = showDropdown,
+        onExpandedChange = {showDropdown = it }
+    ){
+        TextField(
+            value = itemList[selectedIndex],
             modifier = modifier
-                .background(MaterialTheme.colorScheme.onSecondary)
-                .clickable { showDropdown = true },
-//            .clickable { showDropdown = !showDropdown },
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = itemList[selectedIndex], modifier = Modifier.padding(3.dp))
-        }
-
-        // dropdown list
-        Box() {
-            if (showDropdown) {
-                Popup(
-                    alignment = Alignment.TopCenter,
-                    properties = PopupProperties(
-                       // excludeFromSystemGesture = true,
-                    ),
-                    // to dismiss on click outside
-                    onDismissRequest = { showDropdown = false }
-                ) {
-
-                    Column(
-                        modifier = modifier
-                            .heightIn(max = 90.dp)
-                            .verticalScroll(state = scrollState)
-                            .border(width = 1.dp, color = MaterialTheme.colorScheme.onBackground),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                    ) {
-
-                        itemList.onEachIndexed { index, item ->
-                            if (index != 0) {
-                                Divider(thickness = 1.dp, color = MaterialTheme.colorScheme.onSecondary)
-                            }
-                            Box(
-                                modifier = Modifier
-                                    .background(MaterialTheme.colorScheme.onBackground)
-                                    .fillMaxWidth()
-                                    .clickable {
-                                        onItemClick(index)
-                                        showDropdown = !showDropdown
-                                    },
-                                contentAlignment = Alignment.Center
-                            ) {
-                                Text(text = item, color = MaterialTheme.colorScheme.onSecondary)
-                            }
-                        }
-
-                    }
-                }
+                .menuAnchor(),
+            readOnly = true,
+            onValueChange = {},
+            label = {},
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = showDropdown)},
+        )
+        ExposedDropdownMenu(
+            expanded = showDropdown,
+            onDismissRequest = {showDropdown = false}
+        ){
+            itemList.forEachIndexed{index, item ->
+                DropdownMenuItem(
+                    text = { Text(item) },
+                    onClick = {
+                        selectedIndex = index
+                        showDropdown = false
+                    },
+                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                )
             }
+
         }
     }
-
 }
+
+
