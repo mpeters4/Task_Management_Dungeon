5a70da08ceac4c7fd57887c3bba436c4771ca019
Michi <50985676+mpeters4@users.noreply.github.com>
2024-03-28 11:59:12 +0100

    Update EditProjectScreen.kt
    moving to laptop. NOT STABLE

--- Task_Management_Dungeon/src/main/kotlin/screen/EditProjectScreen.kt
+++ Task_Management_Dungeon/src/main/kotlin/screen/EditProjectScreen.kt
@@ -6,12 +6,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,7 +17,7 @@ import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
-import classes.Project
+import classes.*
 import com.example.compose.AppTheme
 import composable.bodyText
 import composable.dropdownSelection
@@ -27,13 +25,30 @@ import composable.expandableItem
 import composable.title
 import icon.addIcon
 
-class EditProjectScreen(val project: Project) : Screen {
+class EditProjectScreen(var project: Project) : Screen {
     @Composable
     @Preview
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
         val dependencies = mutableStateListOf("Sequenz", "Wenn Richtig, dann", "Wenn Falsch, dann", "Optionale Unteraufgabe", "Pflicht Unteraufgabe")
+        var projectDependencyList = remember { mutableStateListOf<Dependency>() }
         var selectedIndex by remember { mutableStateOf(0) }
+        projectDependencyList.add(
+            Dependency(SingleChoiceQuestion(
+                "Dies ist eine Testfrage, wobei Antwort 2 dieee Lösung ist",
+                1, 1, "Die Erklärung hierzu ist echt nicht nötig",
+                listOf("Antwort 1", "Antwort 2", "antwoort3"), listOf("tag 1", "tag2"), correctAnswerIndex = 1
+            ),
+                MultipleChoiceQuestion(
+                    "AFRAGE",
+                    1,
+                    1,
+                    "Erklärung",
+                    listOf("Antwort 1", "a2", "a3"),
+                    listOf("t1", "a", "t3"),
+                    correctAnswerIndices = listOf(1,2)
+        ), DependencyType.SEQUENCE))
+
         AppTheme {
             Surface(
                 modifier = Modifier.fillMaxSize(),
@@ -54,7 +69,7 @@ class EditProjectScreen(val project: Project) : Screen {
                                 onClick = {
                                     navigator.pop()
                                 }) {
-                                androidx.compose.material3.Text("Zurück")
+                                Text("Zurück")
                             }
                         }
                     }
@@ -62,27 +77,36 @@ class EditProjectScreen(val project: Project) : Screen {
                     LazyColumn(Modifier.padding(it)) {
                         item {
                             title("Spielablauf:")
+                            bodyText("Bitte wählen Sie Fragen aus, die in das Spiel integriert werden sollen. Jede Frage muss in Abhängigkeit zu einer anderen Frage stehen. Die Art der Abhängigkeiten kann dabei frei gewählt werden. Hat eine Frage mehrere Abhängigkeiten, muss die Frage mehrmals ausgewählt und zugeordnet werden.")
                         }
-                        item {
-                            dropdownSelection(dependencies, selectedIndex = selectedIndex, modifier = Modifier, {})
-
-                        }
-                        items(items = project.dependencies) {dependency ->
+                        items(items = projectDependencyList) {dependency ->
                             Row {
-                                expandableItem(dependency.questionA,{})
-                                dropdownSelection(dependencies, selectedIndex = selectedIndex, modifier = Modifier, {})
-                                expandableItem(dependency.questionB,{})
+                                if (dependency.questionA != null){
+                                    expandableItem(question = dependency.questionA, action = { navigator.push(QuestionChooserScreen()) }, modifier = Modifier.weight(3f))
+                                }else{
+                                    Text("Frage hinzufügen", modifier = Modifier.clickable { navigator.push(QuestionChooserScreen()) })
+                                }
+                                dropdownSelection(dependencies, modifier = Modifier, {})
+                                if (dependency.questionB != null){
+                                    expandableItem(question = dependency.questionB, action = { navigator.push(QuestionChooserScreen()) }, modifier = Modifier.weight(3f))
+                                }else{
+                                    Text("Frage hinzufügen", modifier = Modifier.weight(1f).clickable { navigator.push(QuestionChooserScreen(),) })
+                                }
+
                             }
-
-
                         }
 
-                        item { Row(Modifier.padding(start = 24.dp, end = 24.dp).fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                        item { Row(Modifier.padding(start = 24.dp, end = 24.dp).fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
                             Image(
                                 addIcon(androidx.compose.material3.MaterialTheme.colorScheme.onBackground),
                                 "Add",
                                 Modifier.padding(4.dp, top = 26.dp).clickable {
-                                    navigator.push(QuestionChooserScreen())
+                                    //navigator.push(QuestionChooserScreen())
+                                    //localProject.dependencies.add(Dependency(null,null, dependency = DependencyType.SEQUENCE))
+                                    projectDependencyList.add(Dependency(null,null, dependency = DependencyType.SEQUENCE))
+                                    project.dependencies = projectDependencyList
+                                    //add(Dependency(null,null, dependency = DependencyType.SEQUENCE))
+
                                 })
                             bodyText("Neue Frage hinzufügen")
                         } }
