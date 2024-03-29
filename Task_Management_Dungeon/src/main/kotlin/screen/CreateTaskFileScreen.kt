3b20b083dfefde4a94328d3df3b618e8c0f5d0f2
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-03-29 17:13:16 +0100

    Update CreateTaskFileScreen.kt

--- Task_Management_Dungeon/src/main/kotlin/screen/CreateTaskFileScreen.kt
+++ Task_Management_Dungeon/src/main/kotlin/screen/CreateTaskFileScreen.kt
@@ -1,56 +1,111 @@
 package screen
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import classes.Project
 import com.example.compose.AppTheme
+import composable.bodyText
 import composable.title
+import kotlinx.coroutines.launch
 
 class CreateTaskFileScreen : Screen {
     @Composable
     @Preview
     override fun Content() {
+        var projects = remember { mutableStateListOf<Project>() }
+        val snackbarHostState = remember { SnackbarHostState() }
+        val scope = rememberCoroutineScope()
+        val selectedIndices = remember { mutableStateListOf<Int>() }
+        projects.add(Project(name = "Projekt A"))
+        projects.add(Project(name = "Projekt B"))
+        projects.add(Project(name = "Mathe Oberstufe"))
+
         val navigator = LocalNavigator.currentOrThrow
         AppTheme {
             Surface(
                 modifier = Modifier.fillMaxSize(),
-                color = androidx.compose.material3.MaterialTheme.colorScheme.background,
+                color = MaterialTheme.colorScheme.background,
             ) {
                 Scaffold(
+                    snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
                     topBar = {
-                        title("Aufgabendatei erstellen")
-                    },
+                        Column {
+                            title("Aufgabendatei erstellen")
+                            bodyText("Bitte wählen Sie die Projekte, die in die Aufgabendatei aufgenommen werden sollen")
+                        }
+                     },
                     bottomBar = {
                         Row(//verticalAlignment = Alignment.Bottom,
                             modifier = Modifier.fillMaxWidth(),
                             horizontalArrangement = Arrangement.Start
                         ) {
-                            androidx.compose.material3.Button(
+                            Button(
                                 modifier = Modifier.padding(16.dp),
                                 colors = ButtonDefaults.buttonColors(),
                                 onClick = {
-                                    navigator.pop()
+                                   navigator.pop()
                                 }) {
                                 Text("Zurück")
                             }
+                            Button(
+                                modifier = Modifier.padding(16.dp),
+                                colors = ButtonDefaults.buttonColors(),
+                                onClick = {
+                                    if (selectedIndices.isEmpty()) {
+                                        scope.launch {
+                                            snackbarHostState.showSnackbar(
+                                                message = "Bitte wählen Sie mindestens eine korrekte Antwort aus",
+                                                withDismissAction = true
+                                            )
+                                        }
+                                    } else {navigator.pop()}
+                                }) {
+                                Text("Datei Speichern")
+                            }
                         }
                     }
-
                 ) {
+                    LazyColumn(
+                        Modifier.padding(it).fillMaxSize()
+                    ) {
+                        itemsIndexed(items = projects) { index, project ->
+                            bodyText(project.name, modifier = Modifier.selectable(
+                                selected = selectedIndices.contains(index),
+                                onClick = {
+                                    if (!selectedIndices.contains(index)) {
+                                        selectedIndices.add(index)
+                                    } else {
+                                        selectedIndices.remove(index)
+                                    }
+                                }
+                            ).clip(shape = RoundedCornerShape(10.dp)).background(
+                                if (selectedIndices.contains(index)) {
+                                    MaterialTheme.colorScheme.onSecondary
+                                } else MaterialTheme.colorScheme.background
+                            ).fillParentMaxWidth().padding(start = 16.dp, end = 16.dp, top = 16.dp))
+                            Spacer(modifier = Modifier.padding(8.dp))
+                        }
+
+                    }
                 }
 
             }
         }
-
     }
 }
-
