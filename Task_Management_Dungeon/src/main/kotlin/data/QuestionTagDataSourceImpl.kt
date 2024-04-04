ff9be72b8044589add5907e89e7a51dd8cb916bd
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-04-04 11:55:34 +0200

    Update QuestionTagDataSourceImpl.kt

--- Task_Management_Dungeon/src/main/kotlin/data/QuestionTagDataSourceImpl.kt
+++ Task_Management_Dungeon/src/main/kotlin/data/QuestionTagDataSourceImpl.kt
@@ -1,13 +1,17 @@
 package data
 
 import Task_Management_Dungeon.Database
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
 import db.Question
 import db.Tag
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 
 class QuestionTagDataSourceImpl(db : Database): QuestionTagDataSource {
-    override fun getTagsByQuestionId(questionId: Long): Flow<List<Tag>> {
-        TODO("Not yet implemented")
+    private val queries = db.questionTagQueries
+    override fun getTagsByQuestionId(questionId: Long): Flow<List<String>> {
+        return queries.getTagsByQuestionId(questionId).asFlow().mapToList(Dispatchers.IO)
     }
 
     override fun getQuestionsByTag(): Flow<List<Question>> {
