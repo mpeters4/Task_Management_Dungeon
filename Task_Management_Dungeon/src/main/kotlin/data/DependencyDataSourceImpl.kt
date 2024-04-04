c431ef648d2ed3f0dc8585d0ed0649336118a1cf
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-04-04 13:16:08 +0200

    implementation

--- Task_Management_Dungeon/src/main/kotlin/data/DependencyDataSourceImpl.kt
+++ Task_Management_Dungeon/src/main/kotlin/data/DependencyDataSourceImpl.kt
@@ -1,16 +1,23 @@
 package data
 
 import Task_Management_Dungeon.Database
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
 import db.Dependency
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 
 class DependencyDataSourceImpl(db: Database): DependencyDataSource {
+    private val queries = db.dependencyQueries
     override suspend fun getDependencyById(id: Long): Dependency? {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO){
+            queries.getDependencyById(id).executeAsOneOrNull()
+        }
     }
 
     override fun getAllDependenciesByProjectId(projectId: Long): Flow<List<Dependency>> {
-        TODO("Not yet implemented")
+        return queries.getAllDependenciesByProjectId(projectId).asFlow().mapToList(Dispatchers.IO)
     }
 
     override suspend fun insertDependency(
@@ -21,10 +28,14 @@ class DependencyDataSourceImpl(db: Database): DependencyDataSource {
         dependency: String,
         id: Long?
     ) {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO){
+            queries.insertDependency(id,questionAId,questionBId,projectId,position,dependency)
+        }
     }
 
     override suspend fun deleteDependencyById(id: Long) {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO){
+            queries.deleteDependencyById(id)
+        }
     }
 }
