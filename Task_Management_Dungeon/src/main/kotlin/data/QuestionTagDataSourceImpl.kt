dc15869cef7dc19d41fade8f28320da89160215a
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-04-04 13:16:36 +0200

    implementation

--- Task_Management_Dungeon/src/main/kotlin/data/QuestionTagDataSourceImpl.kt
+++ Task_Management_Dungeon/src/main/kotlin/data/QuestionTagDataSourceImpl.kt
@@ -4,9 +4,9 @@ import Task_Management_Dungeon.Database
 import app.cash.sqldelight.coroutines.asFlow
 import app.cash.sqldelight.coroutines.mapToList
 import db.Question
-import db.Tag
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 
 class QuestionTagDataSourceImpl(db : Database): QuestionTagDataSource {
     private val queries = db.questionTagQueries
@@ -14,15 +14,16 @@ class QuestionTagDataSourceImpl(db : Database): QuestionTagDataSource {
         return queries.getTagsByQuestionId(questionId).asFlow().mapToList(Dispatchers.IO)
     }
 
-    override fun getQuestionsByTag(): Flow<List<Question>> {
-        TODO("Not yet implemented")
-    }
 
     override suspend fun insertQuestionTag(questionId: Long, tagId: Long) {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO){
+            queries.insertQuestionTag(questionID = questionId, tagID =  tagId )
+        }
     }
 
     override suspend fun deleteQuestionTag(questionId: Long, tagId: Long) {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO){
+            queries.deleteQuestionTag(questionId = questionId, tagId = tagId )
+        }
     }
 }
