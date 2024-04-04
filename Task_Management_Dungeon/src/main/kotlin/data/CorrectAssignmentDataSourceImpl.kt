64d9eaeec7dec3b0daf6987245489df6e9670e41
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-04-04 13:50:34 +0200

    Answer Impl

--- Task_Management_Dungeon/src/main/kotlin/data/CorrectAssignmentDataSourceImpl.kt
+++ Task_Management_Dungeon/src/main/kotlin/data/CorrectAssignmentDataSourceImpl.kt
@@ -2,17 +2,26 @@ package data
 
 import Task_Management_Dungeon.Database
 import db.Answer
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class CorrectAssignmentDataSourceImpl(db:Database): CorrectAnswerDataSource {
+    private val queries = db.correctAnswerQueries
     override suspend fun getCorrectAnswerByQuestionId(questionId: Long): Answer? {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO){
+            queries.getCorrectAnswerByQuestionId(questionId).executeAsOneOrNull()
+        }
     }
 
     override suspend fun insertCorrectAnswer(questionId: Long, answerId: Long) {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO){
+            queries.insertCorrectAnswer(questionID = questionId, answerID = answerId)
+        }
     }
 
     override suspend fun deleteCorrectAnswerByAnswerId(answerId: Long, questinId: Long) {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO){
+            queries.deleteCorrectAnswerByAnswerId(answerId = answerId, questionId = questinId)
+        }
     }
 }
