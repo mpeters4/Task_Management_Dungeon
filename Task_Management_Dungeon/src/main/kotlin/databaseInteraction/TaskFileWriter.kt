b5d8246f82096f1662279cca0b606ccf5b811961
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-04-07 19:29:51 +0200

    Create TaskFileWriter.kt

---
+++ Task_Management_Dungeon/src/main/kotlin/databaseInteraction/TaskFileWriter.kt
@@ -0,0 +1,18 @@
+package databaseInteraction
+
+import classes.Question
+import java.io.File
+
+
+object TaskFileWriter {
+
+
+
+    fun writeQuestion(question: Question){
+        File("Test.dng")
+
+    }
+
+
+
+}
