5141c3efc3f9d5456b4ba2843de2d5cf66ad84ef
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2024-04-08 09:55:53 +0200

    Update Question.kt
    added Id for better handling

--- Task_Management_Dungeon/src/main/kotlin/classes/Question.kt
+++ Task_Management_Dungeon/src/main/kotlin/classes/Question.kt
@@ -3,6 +3,7 @@ package classes
 import androidx.compose.runtime.mutableStateListOf
 
 open class Question(
+    val id: Long = 0,
     var description: String = "",
     var points: Int = 0,
     var pointsToPass: Int = 0,
